53daf96e46cfa3a13c7df740c9ecb1bb7d80f217
Graham Ashton <graham@effectif.com>
2022-03-16 16:18:17 +0000

    Configure the pyright language server for Python
    I'm following along with TJ and Bashbunni's Bash2Basics video series. [1]
    This config is based on their Go setup from the first video, though I
    tweaked a couple of the mappings slightly.
    
    Most of these mappings work well in both gopls and pyright, but pyright
    doesn't support `vim.lsp.buf.implementation()`, which lists implementations
    for the symbol under the cursor.
    
    [1] https://www.youtube.com/playlist?list=PLep05UYkc6wTWlugE_9Lj6JlLpvSBbkZ_

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,11 +1,21 @@
+function default_lsp_keymaps()
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+  vim.keymap.set("n", "ctrl-]", vim.lsp.buf.definition, {buffer=0})
+  vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+  vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {buffer=0})
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {buffer=0})
+end
+
 require'lspconfig'.gopls.setup{
   on_attach = function()
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-    vim.keymap.set("n", "ctrl-]", vim.lsp.buf.definition, {buffer=0})
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+    default_lsp_keymaps()
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {buffer=0})
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {buffer=0})
+  end,
+}
+
+require'lspconfig'.pyright.setup{
+  on_attach = function()
+    default_lsp_keymaps()
   end,
 }
