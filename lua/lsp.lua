182037ba9812d1a189ad02c74bb3237a94e9d1ea
Graham Ashton <graham@effectif.com>
2022-03-24 16:27:10 +0000

    Configure lsp servers in a loop
    I've also set the debounce_text_changes flag to the value suggested in
    the help. Note that the README says this will become the default from
    Neovim 0.7.

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,40 +1,26 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-function default_lsp_keymaps()
+function lsp_keymaps()
   vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
   vim.keymap.set("n", "ctrl-]", vim.lsp.buf.definition, {buffer=0})
   vim.keymap.set("n", ":Ref<CR>", vim.lsp.buf.references, {buffer=0})
   vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
   vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
   vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {buffer=0})
   vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {buffer=0})
 end
 
-require'lspconfig'.ansiblels.setup{
-  capabilities = capabilities,
-  on_attach = function()
-    default_lsp_keymaps()
-  end,
-}
+local servers = {'ansiblels', 'bashls', 'gopls', 'pyright'}
 
-require 'lspconfig'.bashls.setup{
-  capabilities = capabilities,
-  on_attach = function()
-    default_lsp_keymaps()
-  end,
-}
-
-require'lspconfig'.gopls.setup{
-  capabilities = capabilities,
-  on_attach = function()
-    default_lsp_keymaps()
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-  end,
-}
-
-require'lspconfig'.pyright.setup{
-  capabilities = capabilities,
-  on_attach = function()
-    default_lsp_keymaps()
-  end,
-}
+for _, server in pairs(servers) do
+  require'lspconfig'[server].setup {
+    capabilities = capabilities,
+    on_attach = function()
+      lsp_keymaps()
+    end,
+    flags = {
+      debounce_text_changes = 150,
+    },
+  }
+end
