10b2eec9ad73d9dbf8ae5840595dc69af8825bb2
Graham Ashton <graham@effectif.com>
2023-04-22 22:02:26 +0100

    Re-organise Telescope mappings

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -5,13 +5,16 @@ require"telescope".load_extension("fzf")
 
 vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope git_files<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>B", ":Telescope file_browser sorting_strategy=ascending<CR><CR>", { noremap = true })
+
 vim.api.nvim_set_keymap("n", "<leader>b", ":Telescope buffers<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fl", ":Telescope current_buffer_fuzzy_find sorting_strategy=ascending<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fc", ":Telescope git_commits<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope git_bcommits<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fC", ":Telescope commands<CR>", { noremap = true })
+
 vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>ft", ":Telescope tags<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fC", ":Telescope commands<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fm", ":Telescope keymaps<CR>", { noremap = true })
 
-vim.api.nvim_set_keymap("n", "<leader>B", ":Telescope file_browser sorting_strategy=ascending<CR><CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>ft", ":Telescope tags<CR>", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "<leader>fc", ":Telescope git_commits<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope git_bcommits<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fl", ":Telescope current_buffer_fuzzy_find sorting_strategy=ascending<CR>", { noremap = true })
