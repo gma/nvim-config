3b2c9ec2ec9c2b9e30ec316779faf96aef371ded
Graham Ashton <graham@effectif.com>
2023-04-24 11:35:27 +0100

    Migrate FileType autocommands from dotvim

---
+++ after/ftplugin/mail.lua
@@ -0,0 +1,4 @@
+vim.opt_local.joinspaces = false  -- don't insert two spaces after "."
+vim.opt_local.linebreak = true    -- wrap on word boundaries
+vim.opt_local.smartindent = false
+vim.opt_local.wrap = true
