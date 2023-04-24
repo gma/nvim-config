b5760f509ece5867a6aefa397a0e0a6c673a9ea4
Graham Ashton <graham@effectif.com>
2023-04-24 18:04:41 +0100

    Install Prime's Harpoon plugin
    I've setup keymaps to allow me to jump to files by number with `<leader>n`
    or to cycle between them with `<Alt-]>` and `<Alt-[>` (next and previous).
    
    Show and hide the Harpoon list with `<A-p>.`
    
    Add a file to the project's Harpoon list with `<C-A-p>.` Remove a file
    by opening the list window and deleting it (it's a normal buffer).

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,4 +38,23 @@ return {
     "nvim-lua/plenary.nvim",
     lazy = true,
   },
+  {
+    "theprimeagen/harpoon",
+    config = function()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+
+      vim.keymap.set("n", "<C-A-p>", mark.add_file, { desc = "Add file to Harpoon" })
+      vim.keymap.set("n", "<A-p>", ui.toggle_quick_menu, { desc = "Toggle Harpoon quick menu" })
+      vim.keymap.set("n", "<A-[>", function() ui.nav_prev() end, { desc = "Previous Harpoon file" })
+      vim.keymap.set("n", "<A-]>", function() ui.nav_next() end, { desc = "Next Harpoon file" })
+      vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Harpoon file 1" })
+      vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Harpoon file 2" })
+      vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = "Harpoon file 3" })
+      vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { desc = "Harpoon file 4" })
+      vim.keymap.set("n", "<leader>5", function() ui.nav_file(5) end, { desc = "Harpoon file 5" })
+      vim.keymap.set("n", "<leader>6", function() ui.nav_file(6) end, { desc = "Harpoon file 6" })
+    end,
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 }
