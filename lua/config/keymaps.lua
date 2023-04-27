f0141c7155c0bbb2ca8504fc989e2b2168f757a6
Graham Ashton <graham@effectif.com>
2023-04-27 22:44:10 +0100

    Define set function to tidy up keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,16 @@
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to window to left" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to window above" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to window below" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to window to right" })
+local set = vim.keymap.set
+
+set("n", "<C-h>", "<C-w>h", { desc = "Move to window to left" })
+set("n", "<C-j>", "<C-w>j", { desc = "Move to window above" })
+set("n", "<C-k>", "<C-w>k", { desc = "Move to window below" })
+set("n", "<C-l>", "<C-w>l", { desc = "Move to window to right" })
 
 vim.opt.listchars = { tab = '>-', trail = "·", eol = "$", extends = "#", nbsp = "·" }
-vim.keymap.set("n", "<leader>s", function()
+set("n", "<leader>s", function()
   vim.o.list = not vim.o.list
 end, { desc = "Show / hide whitespace", silent = true })
 
-vim.keymap.set("c", "%%", function()
+set("c", "%%", function()
   if vim.fn["getcmdtype"]() == ":" then
     return vim.fn["expand"]("%:h") .. "/"
   else
