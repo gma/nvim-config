4871c86da63fd0c2625a6e23a59d11ca66a64820
Graham Ashton <graham@effectif.com>
2023-04-27 23:17:57 +0100

    Add Prime's system clipboard yank keymaps
    The first one is to be followed by a movement command. The second
    obviously works in visual mode. The third yanks complete lines.
    
    https://youtu.be/w7i4amO_zaE?t=1633

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,6 +20,10 @@ set("n", "J", "mzJ`z", { desc = "Join lines" })
 set("v", "K", ":move '<-2<cr>gv=gv", { desc = "Move selected lines up" })
 set("v", "J", ":move '>+1<cr>gv=gv", { desc = "Move selected lines down" })
 
+set("n", "<leader>y", "\"+y", { desc = "Yank to system clipboard" })
+set("v", "<leader>y", "\"+y", { desc = "Yank to system clipboard" })
+set("n", "<leader>Y", "\"+Y", { desc = "Yank lines to system clipboard" })
+
 -- Display
 
 vim.opt.listchars = { tab = '>-', trail = "·", eol = "$", extends = "#", nbsp = "·" }
