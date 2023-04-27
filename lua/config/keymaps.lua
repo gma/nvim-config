1a9d115a12cc0bab4cd43fb8e71bae4f0b6daccd
Graham Ashton <graham@effectif.com>
2023-04-27 22:55:19 +0100

    Add Prime's visual move keymaps
    I'd have used [e and ]e but they're already defined by the unimpaired
    plugin, and work slightly differently. They support a count, and can be
    repeated with . but they don't update the indent, so these are arguably
    better in general use.
    
    https://youtu.be/w7i4amO_zaE?t=1534

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,25 @@
 local set = vim.keymap.set
 
+-- Navigation
+
 set("n", "<C-h>", "<C-w>h", { desc = "Move to window to left" })
 set("n", "<C-j>", "<C-w>j", { desc = "Move to window above" })
 set("n", "<C-k>", "<C-w>k", { desc = "Move to window below" })
 set("n", "<C-l>", "<C-w>l", { desc = "Move to window to right" })
 
+-- Editing
+
+set("v", "K", ":move '<-2<cr>gv=gv", { desc = "Move selected lines up" })
+set("v", "J", ":move '>+1<cr>gv=gv", { desc = "Move selected lines down" })
+
+-- Display
+
 vim.opt.listchars = { tab = '>-', trail = "·", eol = "$", extends = "#", nbsp = "·" }
 set("n", "<leader>s", function()
   vim.o.list = not vim.o.list
 end, { desc = "Show / hide whitespace", silent = true })
+
+-- Other
 
 set("c", "%%", function()
   if vim.fn["getcmdtype"]() == ":" then
