6b8c58507a36e0770ca0fee5791c6df5f23afc70
Graham Ashton <graham@effectif.com>
2023-04-30 20:03:04 +0100

    Use block cursor in insert mode
    The default value of guicursor is:
    
        n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20
    
    I've just removed "i-" from the second part, to switch back to the block
    cursor from the thin bar in insert mode.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -52,6 +52,7 @@ vim.g.netrw_banner = 0
 -- The settings higher up in this file were brought over from my Vim 8
 -- config. The following are only supported by Neovim.
 
+opt.guicursor = "n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20"
 opt.laststatus = 2            -- enable global statusline
 opt.signcolumn = "yes"        -- show space where signs appear in the gutter
 opt.termguicolors = true      -- enable 24-bit colour
