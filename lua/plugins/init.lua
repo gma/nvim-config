1eb1fab6d7631acc3d44a8604dcdbbb9e35ad00f
Graham Ashton <graham@effectif.com>
2023-05-02 14:25:50 +0100

    Move numbered Harpoon keymaps to Alt key
    This seems much more natural to me, and has the added benefit of not
    clobbering my existing Markdown h1 and h2 keymaps.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,12 +34,12 @@ return {
       vim.keymap.set("n", "<A-p>", ui.toggle_quick_menu, { desc = "Toggle Harpoon quick menu" })
       vim.keymap.set("n", "<A-[>", function() ui.nav_prev() end, { desc = "Previous Harpoon file" })
       vim.keymap.set("n", "<A-]>", function() ui.nav_next() end, { desc = "Next Harpoon file" })
-      vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Harpoon file 1" })
-      vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Harpoon file 2" })
-      vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = "Harpoon file 3" })
-      vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { desc = "Harpoon file 4" })
-      vim.keymap.set("n", "<leader>5", function() ui.nav_file(5) end, { desc = "Harpoon file 5" })
-      vim.keymap.set("n", "<leader>6", function() ui.nav_file(6) end, { desc = "Harpoon file 6" })
+      vim.keymap.set("n", "<A-1>", function() ui.nav_file(1) end, { desc = "Harpoon file 1" })
+      vim.keymap.set("n", "<A-2>", function() ui.nav_file(2) end, { desc = "Harpoon file 2" })
+      vim.keymap.set("n", "<A-3>", function() ui.nav_file(3) end, { desc = "Harpoon file 3" })
+      vim.keymap.set("n", "<A-4>", function() ui.nav_file(4) end, { desc = "Harpoon file 4" })
+      vim.keymap.set("n", "<A-5>", function() ui.nav_file(5) end, { desc = "Harpoon file 5" })
+      vim.keymap.set("n", "<A-6>", function() ui.nav_file(6) end, { desc = "Harpoon file 6" })
     end,
     dependencies = { "nvim-lua/plenary.nvim" },
   },
