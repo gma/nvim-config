6eda47d149dd57bee044291efb3dec8e7388fc2d
Graham Ashton <graham@effectif.com>
2023-12-05 12:29:44 +0000

    Add support for auto-formatting Python with ruff
    See commit 0976a35 for more info on this technique.

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -3,7 +3,11 @@ vim.g.black_linelength = 80
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = vim.api.nvim_create_augroup("format_python", { clear = true }),
   callback = function(match)
-    if vim.fn.filereadable(".use-blue") == 1 then
+    if vim.fn.filereadable(".use-ruff") == 1 then
+      vim.cmd.write()
+      vim.fn.system("ruff format " .. match.file)
+      vim.cmd.edit({ bang = true })
+    elseif vim.fn.filereadable(".use-blue") == 1 then
       vim.cmd.write()
       vim.fn.system("blue " .. match.file)
       vim.cmd.edit({ bang = true })
