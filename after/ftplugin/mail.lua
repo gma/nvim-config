8c89e1cf6fc6e815fcb6bf98035bb7e978f9efe4
Graham Ashton <graham@effectif.com>
2024-08-16 11:23:32 +0100

    Disable hard wrapping mail mode
    When writing emails I've always enjoyed (preferred) hard wrapping, as
    that is a traditional default. Modern mail clients don't do it though,
    and in order to gain some more consistency with emails that I write in
    the Fastmail user interface, I'm disabling hard wrapping for mails
    written in Vim too.

--- after/ftplugin/mail.lua
+++ after/ftplugin/mail.lua
@@ -1,4 +1,5 @@
 vim.opt_local.joinspaces = false  -- don't insert two spaces after "."
 vim.opt_local.linebreak = true    -- wrap on word boundaries
 vim.opt_local.smartindent = false
-vim.opt_local.wrap = true
+vim.opt_local.textwidth = 0
+vim.opt_local.wrap = true         -- soft wrap (as textwidth is 0)
