92f6c3bd0516d17f5995028edcab70ab967f2e1b
Graham Ashton <graham@effectif.com>
2024-11-14 14:05:01 +0000

    Replace base16 theme and airline with catpuccin
    I decided to give this a try while watching typecraft's video on setting
    up Neovim for Rails [1].
    
    I'm liking the colours on the desktop a lot, and am appreciating a
    clearer status bar (with a lot more room for the name of the file I'm
    working in; very useful when copying/pasting the current filename into
    the terminal with tmux keyboard shortcuts!).
    
    [1] https://www.youtube.com/watch?v=cWkX4m5xyiY

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,28 +1,9 @@
-local function colorscheme_theme()
-  local config = vim.fn.expand("~/.config/base16-theme")
-  return "base16-" .. vim.fn.readfile(config)[1]
-end
-
-local function airline_theme()
-  return table.concat(vim.split(colorscheme_theme(), "-"), "_")
-end
-
 return {
-  {
-    "fnune/base16-vim",
-    config = function()
-      vim.g.base16_colorspace = 256
-      vim.cmd.colorscheme(colorscheme_theme())
-      vim.cmd.highlight("WinSeparator guifg=#444444 guibg=None")
-    end,
-  },
-  {
-    "vim-airline/vim-airline-themes",
-    config = function()
-      vim.g.airline_theme = airline_theme()
-    end,
-    dependencies = {
-      "vim-airline/vim-airline",
-    },
-  },
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme "catppuccin-mocha"
+  end
 }
