29501fc48ce4d1768958d237ea84073aff7e30c8
Graham Ashton <graham@effectif.com>
2024-12-25 12:36:15 +0000

    Convert Telescope keymaps to functions
    Commands work great, but calling the functions directly feels a little
    cleaner.
    
    I then had to document what all the functions did, which made the code a
    lot more verbose, but the benefit of this is that I get to choose how to
    describe each keymap. That's very useful in which-key's overlay.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,4 @@
+local builtin = require("telescope.builtin")
 local is_inside_work_tree = {}
 
 local git_files_or_find_files = function()
@@ -10,53 +11,127 @@ local git_files_or_find_files = function()
   end
 
   if is_inside_work_tree[cwd] then
-    require("telescope.builtin").git_files(opts)
+    builtin.git_files(opts)
   else
-    require("telescope.builtin").find_files(opts)
+    builtin.find_files(opts)
   end
 end
 
-local run_builtin_grep = function()
-  local builtin = require("telescope.builtin")
-  builtin.grep_string({ search = vim.fn.input("grep: ") })
-end
-
 local define_telescope_keymaps = function()
-  vim.keymap.set("n", "<leader>b", ":Telescope buffers<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader>fl", ":Telescope current_buffer_fuzzy_find sorting_strategy=ascending<cr>", { noremap = true })
+  vim.keymap.set("n", "<leader>b", builtin.buffers, {
+    noremap = true,
+    desc = "Find buffer"
+  })
+  vim.keymap.set("n", "<leader>fl", function()
+    builtin.current_buffer_fuzzy_find({ sorting_strategy = "ascending" })
+  end, {
+  noremap = true,
+  desc = "Find line in buffer"
+})
+  vim.keymap.set("n", "<leader>fT", builtin.builtin, {
+    noremap = true,
+    desc = "Find Telescope builtins"
+  })
+  vim.keymap.set("n", "<C-p>", git_files_or_find_files, {
+    noremap = true,
+    desc = "Find file"
+  })
+  vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+    noremap = true,
+    desc = "Find file (no git)"
+  })
+  vim.keymap.set("n", "<leader><leader>", function()
+    builtin.oldfiles({ only_cwd = true })
+  end, {
+    noremap = true,
+    desc = "Recent files"
+  })
 
-  vim.keymap.set("n", "<leader>fT", ":Telescope builtin<cr>", { noremap = true })
-  vim.keymap.set("n", "<C-p>", git_files_or_find_files, { noremap = true })
-  vim.keymap.set("n", "<leader>ff", ":Telescope find_files<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader><leader>", ":Telescope oldfiles only_cwd=true<cr>", { noremap = true })
+  vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+    noremap = true,
+    desc = "Live grep"
+  })
+  vim.keymap.set("n", "<leader>fG", builtin.grep_string, {
+    noremap = true,
+    desc = "Telescope grep (word)"
+  })
+  vim.keymap.set("n", "<leader>fp", function()
+    builtin.grep_string({ search = vim.fn.input("grep: ") })
+  end, {
+    noremap = true,
+    desc = "Telescope grep (prompt)"
+  })
 
-  vim.keymap.set("n", "<leader>fG", ":Telescope grep_string<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader>fp", run_builtin_grep, { noremap = true, desc = "Telescope grep_string (prompt)" })
-  vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<cr>", { noremap = true })
+  vim.keymap.set("n", "<leader>fM", builtin.marks, {
+    noremap = true,
+    desc = "Search marks"
+  })
+  vim.keymap.set("n", "<leader>fR", builtin.registers, {
+    noremap = true,
+    desc = "Search registers"
+  })
 
-  vim.keymap.set("n", "<leader>fM", ":Telescope marks<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader>fR", ":Telescope registers<cr>", { noremap = true })
+  vim.keymap.set("n", "<leader>ft", builtin.tags, {
+    noremap = true,
+    desc = "Find tags"
+  })
+  vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {
+    noremap = true,
+    desc = "Find LSP references"
+  })
 
-  vim.keymap.set("n", "<leader>ft", ":Telescope tags<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader>fr", ":Telescope lsp_references<cr>", { noremap = true })
+  vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {
+    noremap = true,
+    desc = "Search diagnostics"
+  })
 
-  vim.keymap.set("n", "<leader>fd", ":Telescope diagnostics<cr>", { noremap = true })
+  vim.keymap.set("n", "<leader>fc", builtin.git_commits, {
+    noremap = true,
+    desc = "Search commits"
+  })
+  vim.keymap.set("n", "<leader>fb", builtin.git_bcommits, {
+    noremap = true,
+    desc = "Search buffer's commits"
+  })
+  vim.keymap.set("n", "<leader>fB", builtin.git_branches, {
+    noremap = true,
+    desc = "Browse branches"
+  })
 
-  vim.keymap.set("n", "<leader>fc", ":Telescope git_commits<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader>fb", ":Telescope git_bcommits<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader>fB", ":Telescope git_branches<cr>", { noremap = true })
+  vim.keymap.set("n", "<leader>f<Up>", builtin.command_history, {
+    noremap = true,
+    desc = "Command history"
+  })
+  vim.keymap.set("n", "<leader>f/", builtin.search_history, {
+    noremap = true,
+    desc = "Search history"
+  })
 
-  vim.keymap.set("n", "<leader>f<Up>", ":Telescope command_history<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader>f/", ":Telescope search_history<cr>", { noremap = true })
-
-  vim.keymap.set("n", "<leader>fm", ":Telescope man_pages sections={'ALL'}<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader>fC", ":Telescope commands<cr>", { noremap = true })
-  vim.keymap.set("n", "<leader>fk", ":Telescope keymaps<cr>", { noremap = true })
+  vim.keymap.set("n", "<leader>fm", function()
+    builtin.man_pages({ sections = { "ALL" } })
+  end, {
+    noremap = true,
+    desc = "Find man page"
+  })
+  vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
+    noremap = true,
+    desc = "Search help tags"
+  })
+  vim.keymap.set("n", "<leader>fC", builtin.commands, {
+    noremap = true,
+    desc = "Search Vim commands"
+  })
+  vim.keymap.set("n", "<leader>fk", builtin.keymaps, {
+    noremap = true,
+    desc = "Search keymaps"
+  })
 
   vim.keymap.set("n", "<leader>fn", function()
-    require("telescope.builtin").find_files({ cwd = vim.fn.stdpath('config') })
-  end, { noremap = true, desc = "Find file in Neovim config" })
+    builtin.find_files({ cwd = vim.fn.stdpath('config') })
+  end, {
+    noremap = true,
+    desc = "Find file in Neovim config"
+  })
 end
 
 return {
