e36ac4ec29827d32d3d80cf6e8dbc65eeb29456f
Graham Ashton <graham@effectif.com>
2026-06-26 21:14:01 +0100

    Stop using lsp-zero and nvim-cmp
    lsp-zero has been superfluous for some time. Neovim 0.11 (and later
    0.12) have added great built-in support for both LSP configuration, and
    LSP-based completion.
    
    I'm still using Mason to install servers, and have configured it to
    install my standard set of servers.
    
    Neovim's builtin LSP support requires you to call `vim.lsp.enable()`
    with the names of servers that you want to enable. Rather than doing
    that myself, that's handled by the mason-lspconfig.nvim plugin.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,130 +1,31 @@
-local function setup_lsp()
-  local lsp = require("lsp-zero").preset({
-    manage_nvim_cmp = {
-      set_source = "recommended",
-      set_extra_mappings = true,
-    }
-  })
-
-  local function map(lhs, rhs, desc)
-    vim.keymap.set('n', lhs, rhs, { desc = desc, buffer = true })
-  end
-
-  lsp.on_attach(function(client, bufnr)
-    map("<leader>l", ":LspStop<CR>", "Stop LSP client")
-    map("<leader>L", ":LspStart<CR>", "Start LSP client")
-
-    map("K", vim.lsp.buf.hover, "Show info in window")
-    map("gd", vim.lsp.buf.definition, "Jump to definition")
-    map("gD", vim.lsp.buf.declaration, "Jump to declaration")
-    map("gi", vim.lsp.buf.implementation, "List implementations in quickfix")
-    map("go", vim.lsp.buf.type_definition, "Jump to definition of type")
-    map("gr", vim.lsp.buf.references, "List references in quickfix")
-    map("gh", vim.lsp.buf.signature_help, "Show signature info in window")
-    map("<F2>", vim.lsp.buf.rename, "Rename symbol")
-    map("<F3>", vim.lsp.buf.format, "Format buffer")
-    map("<F4>", vim.lsp.buf.code_action, "Select code action")
-    map("gl", vim.diagnostic.open_float, "Show diagnostic in window")
-    map("[d", vim.diagnostic.goto_prev, "Move to previous diagnostic")
-    map("]d", vim.diagnostic.goto_next, "Move to next diagnostic")
-
-    -- My preferred alternatives to some defaults
-    map("<leader>r", vim.lsp.buf.rename, "Rename symbol")
-    map("g=", vim.lsp.buf.format, "Format buffer")
-    map("<leader>ca", vim.lsp.buf.code_action, "Select code action")
-  end)
-
-  lsp.setup_servers({
-    "ansiblels",
-    "bashls",
-    "cssls",
-    "dockerls",
-    "eslint",
-    "gopls",
-    "html",
-    "lua_ls",
-    "pyright",
-    "rust_analyzer",
-    "vimls",
-    "yamlls",
-  })
-
-  -- nvim_lua_ls() returns Neovim-specific settings for lua_ls
-  require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-
-  lsp.setup()
-end
-
-local function setup_completion()
-  local cmp = require("cmp")
-
-  cmp.setup({
-    sources = {
-      { name = "path" },
-      { name = "nvim_lsp" },
-      { name = "buffer", keyword_length = 4 },
-      { name = "luasnip", keyword_length = 2 },
-      { name = "nvim_lua" },
-    },
-    preselect = "item",
-    mapping = {
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-    },
-  })
-
-  cmp.setup.cmdline("/", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({ { name = "buffer" } }),
-    view = {
-      entries = {
-        name = "wildmenu",
-      },
-    },
-  })
-  cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = "path", },
-    },
-    {
-      { name = "cmdline", keyword_length = 3, },
-    }),
-    view = {
-      entries = {
-        name = "wildmenu",
-      },
-    },
-  })
-end
-
 return {
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v2.x",
-  config = function()
-    setup_lsp()
-    setup_completion()
-  end,
-  dependencies = {
+  {
     {
-      "neovim/nvim-lspconfig",
-      {
-        "williamboman/mason.nvim",
-        build = function()
-          pcall(vim.cmd, "MasonUpdate")
-        end,
-        cmd = "MasonUpdate",
+      "mason-org/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = {
+          "ansiblels",
+          "bashls",
+          "cssls",
+          "dockerls",
+          "eslint",
+          "gopls",
+          "html",
+          "lua_ls",
+          "pyright",
+          "rust_analyzer",
+          "ts_ls",
+          "vimls",
+          "yamlls",
+        },
       },
-      { "williamboman/mason-lspconfig.nvim" },
+      dependencies = {
+        {
+          "mason-org/mason.nvim",
+          opts = {},
+        },
+        "neovim/nvim-lspconfig",
+      },
     },
-
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lua",
-    "hrsh7th/cmp-path",
-
-    "L3MON4D3/LuaSnip",
   },
 }
