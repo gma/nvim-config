6d166924b4e56c57e21311900861f4000ad8dc29
Graham Ashton <graham@effectif.com>
2026-07-04 22:17:25 +0100

    Install Kanagawa colourscheme
    Discovered via @Vhyrro's video.
    
    https://www.youtube.com/watch?v=N-RFCfs6rxI

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,20 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
-  end
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme("catppuccin-mocha")
+    -- end
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("kanagawa-wave")
+    end
+  }
 }
