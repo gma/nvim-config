a506a6b5ba2b6d6b8183b96e57b29963f17cdb60
Graham Ashton <graham@effectif.com>
2026-07-21 20:57:04 +0100

    Re-enable catppuccin mocha theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,19 +2,19 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    -- lazy = false,
-    -- priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme("catppuccin-mocha")
-    -- end
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end
   },
   {
     "rebelot/kanagawa.nvim",
     name = "kanagawa",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("kanagawa-wave")
-    end
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme("kanagawa-wave")
+    -- end
   }
 }
