09ce7c964365612bca15c1005113a35952b9b1c6
Graham Ashton <graham@effectif.com>
2026-08-23 17:30:32 +0100

    Add comment to document Markdown line wrapping
    If you set linebreak and have a non-zero value assigned to textwidth,
    Vim hard-wraps a long line when you press a key. It's a pain in the ass,
    and I have found myself wondering how to disable it (e.g. when editing
    .txt files). So this feels like a useful comment, even though the Neovim
    LSP documents this fact when I pull up the help with `K`.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,4 @@
 vim.opt_local.linebreak = true
-vim.opt_local.textwidth = 0
+vim.opt_local.textwidth = 0     -- disables linebreak's hard-wrap-on-typing
 vim.opt_local.wrap = true
 vim.opt_local.spell = true
