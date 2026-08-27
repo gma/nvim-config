19bb7026a558a5af1f2acb33063f1c44abff739f
Graham Ashton <graham@effectif.com>
2026-08-27 11:30:55 +0100

    Prevent Catppuccin theme from using italics
    I've not noticed the italics before because I've always edited code
    inside a Tmux session, where italics aren't used for some reason.
    Perhaps the different TERM setting (screen-256color vs xterm-kitty) is
    responsible for that.
    
    I've only noticed italics outside of Tmux on keywords in a shell script,
    but I imagine they'd have been used in multiple places.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,6 +5,9 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        no_italic = true,
+      })
       vim.cmd.colorscheme("catppuccin-mocha")
     end
   },
